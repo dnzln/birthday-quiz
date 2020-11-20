fb4301a5d1d820963fa79cc6c2237c33023db01f
Dnzln <qvanoman@gmail.com>
2020-11-20 20:53:38 +0200

    sad

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var currentTab = 0; // Current tab is set to be the first tab (0)
 showTab(currentTab); // Display the current tab
 
+
+
 function showTab(n) {
   // This function will display the specified tab of the form ...
   var x = document.getElementsByClassName("tab");
